1c80fd53b3bbc5e87f4abac561b5cfbae260d4f8
contactluva <138954022+contactluva@users.noreply.github.com>
2025-07-29 12:25:18 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,10 +3,10 @@ self.addEventListener('install', event => {
     caches.open('app-cache-v1').then(cache => {
       return cache.addAll([
         './',
-        './index.html',
+        'https://engdau.vercel.app/eng-dau.html',
         './manifest.json',
-        './icon-192.png',
-        './icon-512.png'
+        'https://github.com/contactluva/bird70/blob/main/dau2.png',
+        'https://github.com/contactluva/bird70/blob/main/dau.png'
       ]);
     })
   );
